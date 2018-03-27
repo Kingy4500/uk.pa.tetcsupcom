8520aecbc6d907c9f0b514f85172887155269e86
Kingy4500 <thekingeffect@hotmail.co.uk>
2018-03-27 21:16:54 +0100

    Add files via upload

---
+++ ui/mods/uk.pa.tetcsupcom/new_game.js
@@ -0,0 +1,7 @@
+!function(){
+
+	var chat_message = "The Commander is an overall better unit with More Armour, More Base Economy, More Build Speed, Its Health Regenerates and last but not least it has a High Velocity, High Powered Long Range Round for an Uber Cannon.";
+
+	model.localChatMessage('The Super Commander Mod is Active',chat_message);
+
+}()
