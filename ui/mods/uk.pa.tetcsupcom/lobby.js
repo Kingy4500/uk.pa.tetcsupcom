ab8d829d761026fd612af68dd24167adfb6f4514
Kingy4500 <thekingeffect@hotmail.co.uk>
2018-03-27 19:05:02 +0100

    Delete lobby.js

--- ui/mods/uk.pa.tetcsupcom/lobby.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	
-	model.localChatMessage('Super Commander', 'Super Commander Mod is enabled on this lobby. ' +
-		'In this mod your Commander is faster, has more armour' +
-		'regenerates health and has a better secondary weapon ' +
-		'The Commander can be an offensive or defensive choice. ' +
-		'Its all about being smart with your commander and your presence on the field. ' +
-		'It is still WIP so please give feedback.');
-})
